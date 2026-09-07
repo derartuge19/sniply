54e1756c9fc52277e15fbca4dde7bdf611c983fc
derartuge19 <derartuge@gmail.com>
2026-09-07 13:32:47 +0300

    adjusting CORS issue

--- sniply_backend/sniply_backend/settings.py
+++ sniply_backend/sniply_backend/settings.py
@@ -145,5 +145,12 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
+# --- CORS ---
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+CORS_ALLOW_CREDENTIALS = True
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
